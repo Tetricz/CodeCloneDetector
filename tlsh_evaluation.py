c3c9e19c665b8b54844642b86eb456a2e200c32b
Orion Lowy <orionlowy@gmail.com>
2023-04-17 14:20:15 -0500

    tlsh_evaluation.py

--- tlsh_evaluation.py
+++ tlsh_evaluation.py
@@ -1,61 +1,27 @@
-# Locality sensitive hashing
-import tlsh
-
-# String similarity
-from fuzzywuzzy import fuzz
-
-# Encoding for converting strings to bytes
-encoding = 'utf-8'
-
-# List of example files
-file_list = [
-    'testfiles/check_polygon.py',
-    'testfiles/check_polygon2.py',
-    'testfiles/testplot.py',
-    'testfiles/testplot2.py'
-]
-
-def hash_and_add_to_list(hash_list, text, text_id):
-    hash_value = tlsh.hash(bytes(text, encoding))
-    hash_list += [(hash_value, text_id)]
-
-def hash_list_from_file_list(file_list):
-    hash_list = []
+from utils import hash_file_compare
+                
+import sys
+                
+def main():
+    # Read list of files to hash
+    input_list_file_name = sys.argv[1]
+    input_list_file = open(input_list_file_name, 'r')
+    input_list = [line for line in input_list_file]
     
-    for file_name in file_list:
-        # Read whole file. This will be replaced by individual function blocks.
-        text = open(file_name, 'r').read()
-        
-        # Hash and add to the list
-        hash_and_add_to_list(hash_list, text, file_name)
+    # Load the input files as a hash_file_compare
+    hfc = hash_file_compare(input_list)
     
-    return hash_list
-    
-def hash_value_similarities(hash_item, hash_list):
-    return [(fuzz.ratio(hash_item[0], other_hash_item[0]) / 100,
-             other_hash_item[1])
-                for other_hash_item in hash_list]
-    
-def main():    
-    hash_list = hash_list_from_file_list(file_list)
-    
-    '''Similar but slightly different files have similar but 
-        slightly different hash values. However, the differences
-        in the hash values can occur anywhere in the hash.
-        
-        So we need to calculate similarities of each hash value 
-        to other hash values independently with ref to each hash value.'''
-        
-    for hash_item in hash_list:
-        hash_list_excluding_current = [x for x in hash_list if x is not hash_item]
-        similarities = hash_value_similarities(hash_item, hash_list_excluding_current)
+    # Report their inter-file similarity scores
+    for file_name in input_list:
+        similarities = hfc.hash_value_similarities(file_name)
         
         key_function = lambda x: x[0]
         similarities = sorted(similarities, key=key_function, reverse=True)
         
-        most_similar = similarities[0]
+        print(f"\nHash similarities to {file_name}:")
         
-        print(f"The most similar file to {hash_item[1]} is {most_similar[1]} with a score of {most_similar[0]}")
+        for s in similarities:
+            print(f"{s[0]}\t{s[1]}")
     
     
 if __name__ == '__main__':
---
+++ utils/hash_file_compare.py
@@ -0,0 +1,25 @@
+# Locality sensitive hashing
+import tlsh
+
+# String similarity
+from fuzzywuzzy import fuzz
+
+class hash_file_compare:
+    def __init__(self, file_list):
+        self.hash_list = []
+        
+        for file_name in file_list:
+            # Read whole file. This will be replaced by individual function blocks.
+            text = open(file_name, 'r').read()
+        
+            # Hash and add to the list
+            hash_value = tlsh.hash(bytes(text, 'utf_8'))
+            self.hash_list += [(hash_value, file_name)]
+        
+    def hash_value_similarities(self, file_name):
+        hash_item = [x for x in self.hash_list if x[1] is file_name][0]
+        hash_list_minus = [x for x in self.hash_list if x[1] is not file_name]
+    
+        return [(fuzz.ratio(hash_item[0], other_hash_item[0]) / 100,
+                    other_hash_item[1])
+                for other_hash_item in hash_list_minus]
