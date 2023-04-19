bf624370cac0d2d08e96412bb0244b54eb42b6e9
Glenntu <glenntu@live.com>
2023-04-19 10:48:17 -0500

    added call to HashFileCompare

--- lazyclonedetector.py
+++ lazyclonedetector.py
@@ -2,12 +2,69 @@ import sys
 import os
 
 from utils.preprocessor import preprocessor
+from utils.hash_file_compare import HashFileCompare
+
+def build_paths_to_targets(target_dir):
+    
+    path_list = []
+
+    cwd = os.getcwd()
+    print(" running in: ",cwd)
+    base_path = cwd + "/" + target_dir
+    
+    dirlist = os.listdir(base_path)
+
+    for ob in dirlist:
+        d = os.path.join(base_path, ob)
+        if os.path.isdir(d):
+            print(d)
+            isin = d.find("preprocessed_")
+            if isin >= 0:
+                flist = os.listdir(d)
+                for f in flist:
+                    full_path = os.path.join(d, f)
+                    if os.path.isfile(os.path.join(d, f)):
+                        #print("      file:   ",f)
+                        ispy = f.find(".py")
+                        
+                        if ispy > 0:
+                             if (len(f) - ispy) == 3:  # it must end in .py, not just contain it
+                                full_path = os.path.join(d, f)
+                                #print("adding to list: ", full_path)
+                                path_list.append(full_path)
+
+    return path_list
+
+def report_similarities(input_list):
+
+    hfc = HashFileCompare(input_list)
+
+    # Report their inter-file similarity scores
+    for file_name in input_list:
+        similarities = hfc.hash_value_similarities(file_name)
+        
+        key_function = lambda x: x[0]
+        similarities = sorted(similarities, key=key_function, reverse=True)
+        
+        print(f"\nHash similarities to {file_name}:")
+        
+        for s in similarities:
+            print(f"{s[0]}\t{s[1]}")
 
 def main(target_dir):
 
     prp = preprocessor()
 
     prp.process_dir(target_dir)
+
+    input_list = build_paths_to_targets(target_dir)
+    print("\n    Generated path list:")
+    for l in input_list:
+        print(" *  ",l)
+    
+    report_similarities(input_list)
+
+    
 
 if __name__ == "__main__":
     print(" argv: ",sys.argv)
